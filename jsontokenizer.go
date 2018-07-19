7e98e3814666a0be3fe6be7a28c6ec8c11ce4e96
Brett Lawson <brett19@gmail.com>
2018-07-19 10:08:32 -0700

    Wrote a new custom JSON tokenizer replacement.
    The old tokenizer was an attempt to reuse the scanner
    included in the stdlib for golang itself.  As the goals
    are different between JSONSM and the stdlib parser,
    there were many opportunities for improvement.  This
    rewritten version has a 2.5x tokenization speed
    improvement which equates to about 2x filtering speed
    increase in the current standard JSONSM benchmark.

--- jsontokenizer.go
+++ jsontokenizer.go
@@ -1,15 +1,20 @@
-// Copyright 2018 Couchbase, Inc. All rights reserved.
-
 package gojsonsm
 
-type TokenType int
+import (
+	"errors"
+	"fmt"
+)
+
+type tokenType int
 
 const (
-	tknUnknown TokenType = iota
+	tknUnknown tokenType = iota
 	tknObjectStart
-	tknArrayStart
 	tknObjectEnd
+	tknObjectKeyDelim
+	tknArrayStart
 	tknArrayEnd
+	tknListDelim
 	tknString
 	tknEscString
 	tknInteger
@@ -20,22 +25,26 @@ const (
 	tknEnd
 )
 
-func isLiteralToken(token TokenType) bool {
+func isLiteralToken(token tokenType) bool {
 	return token >= tknString && token <= tknFalse
 }
 
-func tokenToText(token TokenType) string {
+func tokenToText(token tokenType) string {
 	switch token {
 	case tknUnknown:
 		return "unknown"
 	case tknObjectStart:
 		return "object_start"
-	case tknArrayStart:
-		return "array_start"
 	case tknObjectEnd:
 		return "object_end"
+	case tknObjectKeyDelim:
+		return "object_key_delim"
+	case tknArrayStart:
+		return "array_start"
 	case tknArrayEnd:
 		return "array_end"
+	case tknListDelim:
+		return "list_delim"
 	case tknString:
 		return "string"
 	case tknEscString:
@@ -56,96 +65,421 @@ func tokenToText(token TokenType) string {
 	return "??ERROR??"
 }
 
+type tokenizerState int
+
+const (
+	toksBeginValueOrEmpty tokenizerState = iota
+	toksBeginValue
+	toksBeginStringOrEmpty
+	toksBeginString
+	toksInString
+	toksInStringEsc
+	toksInStringEscU
+	toksInStringEscU1
+	toksInStringEscU12
+	toksInStringEscU123
+	toksNeg
+	toks0
+	toksT
+	toksTr
+	toksTru
+	toksF
+	toksFa
+	toksFal
+	toksFals
+	toksN
+	toksNu
+	toksNul
+	toks1
+	toksDot
+	toksDot0
+	toksE
+	toksESign
+	toksE0
+)
+
+func tokIsSpaceChar(c byte) bool {
+	return c == ' ' || c == '\t' || c == '\r' || c == '\n'
+}
+
 type jsonTokenizer struct {
-	scanner jsonScanner
-	pos     int
-	data    []byte
-
-	replayState int
+	data []byte
+	pos  int
 }
 
-func (tok *jsonTokenizer) start(data []byte) {
-	tok.replayState = scanContinue
-	tok.pos = 0
-	tok.data = data
-	tok.scanner.reset()
+func (tkn *jsonTokenizer) Reset(data []byte) {
+	tkn.data = data
+	tkn.pos = 0
 }
 
-func (tok *jsonTokenizer) seek(pos int) {
-	tok.replayState = scanContinue
-	tok.pos = pos
-	tok.scanner.reset()
+func (tkn *jsonTokenizer) Position() int {
+	return tkn.pos
 }
 
-func (tok *jsonTokenizer) scanStep() int {
-	if tok.pos >= len(tok.data) {
-		return tok.scanner.eof()
+func (tkn *jsonTokenizer) Seek(pos int) {
+	tkn.pos = pos
+}
+
+func (tkn *jsonTokenizer) Step() (tokenType, []byte, error) {
+	// Bring everying local for optimization purposes
+	dataSlice := tkn.data
+	dataLen := len(dataSlice)
+	dataPos := tkn.pos
+
+	// Check that we aren't out of bounds...
+	if dataPos >= dataLen {
+		return tknEnd, nil, nil
 	}
 
-	state := tok.scanner.step(&tok.scanner, tok.data[tok.pos])
-	tok.pos++
-	return state
-}
+	// Keep track of where we started, so we can return the tokens data
+	startPos := dataPos
 
-func (tok *jsonTokenizer) step() (TokenType, []byte, error) {
-	start := tok.pos
+	tokenType := tknUnknown
+	state := toksBeginValue
+	strHasEscapes := false
+	numberIsNonInteger := false
+
+	var c byte
+DataLoop:
 	for {
-		state := tok.scanStep()
+		if dataPos >= dataLen {
+			// Due to the fact that numbers just kind of... end... during JSON parsing
+			// we need some special logic to handle the cases where numbers are involved
+			// and they are the only input to the parser...
+			switch state {
+			case toks1, toks0, toksDot0, toksE0:
+				tokenType = tknNumber
+				break DataLoop
+			case toksBeginValue:
+				tokenType = tknEnd
+				break DataLoop
+			default:
+				// We couldn't have expected this... time to fail :/
+				return tknUnknown, nil, errors.New("unexpected end of input")
+			}
+		}
+
+		c = dataSlice[dataPos]
+		dataPos++
+
 		switch state {
-		case scanError:
-			return 0, nil, tok.scanner.err
-		case scanSkipSpace:
-			start = tok.pos
-		case scanBeginLiteral:
-			for {
-				state := tok.scanStep()
-				if state == scanError {
-					return 0, nil, tok.scanner.err
-				} else if state == scanContinue {
-					continue
-				} else {
-					tok.scanner.undo(state)
-					tok.pos--
-					bytes := tok.data[start:tok.pos]
-					switch tok.scanner.litType {
-					case litString:
-						return tknString, bytes, nil
-					case litEscString:
-						return tknEscString, bytes, nil
-					case litInteger:
-						return tknInteger, bytes, nil
-					case litNumber:
-						return tknNumber, bytes, nil
-					case litNull:
-						return tknNull, bytes, nil
-					case litTrue:
-						return tknTrue, bytes, nil
-					case litFalse:
-						return tknFalse, bytes, nil
-					default:
-						panic("unexpected token literal type")
-					}
-				}
+		case toksBeginValueOrEmpty:
+			if c <= ' ' && tokIsSpaceChar(c) {
+				startPos = dataPos
+				continue DataLoop
+			}
+			if c == ']' {
+				tokenType = tknArrayEnd
+				break DataLoop
+			}
+			fallthrough
+
+		case toksBeginValue:
+			if c <= ' ' && tokIsSpaceChar(c) {
+				startPos = dataPos
+				continue DataLoop
 			}
 
-		case scanBeginObject:
-			return tknObjectStart, nil, nil
-		case scanBeginArray:
-			return tknArrayStart, nil, nil
-		case scanEndObject:
-			return tknObjectEnd, nil, nil
-		case scanEndArray:
-			return tknArrayEnd, nil, nil
-		case scanContinue:
-			panic("unexpected continue")
-		case scanObjectKey:
-			start = tok.pos
-		case scanObjectValue:
-			start = tok.pos
-		case scanArrayValue:
-			start = tok.pos
-		case scanEnd:
-			return tknEnd, nil, nil
+			switch c {
+			case '{':
+				tokenType = tknObjectStart
+				break DataLoop
+			case '}':
+				tokenType = tknObjectEnd
+				break DataLoop
+			case ':':
+				tokenType = tknObjectKeyDelim
+				break DataLoop
+			case '[':
+				tokenType = tknArrayStart
+				break DataLoop
+			case ']':
+				tokenType = tknArrayEnd
+				break DataLoop
+			case ',':
+				tokenType = tknListDelim
+				break DataLoop
+			case '"':
+				state = toksInString
+				continue DataLoop
+			case '-':
+				state = toksNeg
+				continue DataLoop
+			case '0': // beginning of 0.123
+				state = toks0
+				continue DataLoop
+			case 't': // beginning of true
+				state = toksT
+				continue DataLoop
+			case 'f': // beginning of false
+				state = toksF
+				continue DataLoop
+			case 'n': // beginning of null
+				state = toksN
+				continue DataLoop
+			default:
+				if '1' <= c && c <= '9' { // beginning of 1234.5
+					state = toks1
+					continue DataLoop
+				}
+
+				return tknUnknown, nil, fmt.Errorf("looking for beginning of value but found `%c`", c)
+			}
+
+		case toksBeginStringOrEmpty:
+			if c <= ' ' && tokIsSpaceChar(c) {
+				startPos = dataPos
+				continue DataLoop
+			}
+			if c == '}' {
+				tokenType = tknObjectEnd
+				break DataLoop
+			}
+			fallthrough
+
+		case toksBeginString:
+			if c <= ' ' && tokIsSpaceChar(c) {
+				startPos = dataPos
+				continue DataLoop
+			}
+			if c == '"' {
+				state = toksInString
+				continue DataLoop
+			}
+			return tknUnknown, nil, errors.New("looking for beginning of object key string")
+
+		case toksInString:
+			if c == '"' {
+				tokenType = tknEscString
+				break DataLoop
+			}
+			if c == '\\' {
+				state = toksInStringEsc
+				continue DataLoop
+			}
+			if c < 0x20 {
+				return tknUnknown, nil, errors.New("in string literal")
+			}
+
+			// continue with current state
+			continue DataLoop
+
+		case toksInStringEsc:
+			strHasEscapes = true
+
+			switch c {
+			case 'b', 'f', 'n', 'r', 't', '\\', '/', '"':
+				state = toksInString
+				continue DataLoop
+			case 'u':
+				state = toksInStringEscU
+				continue DataLoop
+			default:
+				return tknUnknown, nil, errors.New("in string escape code")
+			}
+
+		case toksInStringEscU:
+			if '0' <= c && c <= '9' || 'a' <= c && c <= 'f' || 'A' <= c && c <= 'F' {
+				state = toksInStringEscU1
+				continue DataLoop
+			}
+			// numbers
+			return tknUnknown, nil, errors.New("in \\u hexadecimal character escape")
+
+		case toksInStringEscU1:
+			if '0' <= c && c <= '9' || 'a' <= c && c <= 'f' || 'A' <= c && c <= 'F' {
+				state = toksInStringEscU12
+				continue DataLoop
+			}
+			// numbers
+			return tknUnknown, nil, errors.New("in \\u hexadecimal character escape")
+
+		case toksInStringEscU12:
+			if '0' <= c && c <= '9' || 'a' <= c && c <= 'f' || 'A' <= c && c <= 'F' {
+				state = toksInStringEscU123
+				continue DataLoop
+			}
+			// numbers
+			return tknUnknown, nil, errors.New("in \\u hexadecimal character escape")
+
+		case toksInStringEscU123:
+			if '0' <= c && c <= '9' || 'a' <= c && c <= 'f' || 'A' <= c && c <= 'F' {
+				state = toksInString
+				continue DataLoop
+			}
+			// numbers
+			return tknUnknown, nil, errors.New("in \\u hexadecimal character escape")
+
+		case toksNeg:
+			if c == '0' {
+				state = toks0
+				continue DataLoop
+			}
+			if '1' <= c && c <= '9' {
+				state = toks1
+				continue DataLoop
+			}
+			return tknUnknown, nil, errors.New("in numeric literal")
+
+		case toks1:
+			if '0' <= c && c <= '9' {
+				state = toks1
+				continue DataLoop
+			}
+			fallthrough
+		case toks0:
+			if c == '.' {
+				state = toksDot
+				continue DataLoop
+			}
+			if c == 'e' || c == 'E' {
+				state = toksE
+				continue DataLoop
+			}
+
+			// need to rewind one character since this was non-numeric
+			dataPos--
+			tokenType = tknNumber
+			break DataLoop
+
+		case toksDot:
+			numberIsNonInteger = true
+
+			if '0' <= c && c <= '9' {
+				state = toksDot0
+				continue DataLoop
+			}
+			return tknUnknown, nil, errors.New("after decimal point in numeric literal")
+
+		case toksDot0:
+			if '0' <= c && c <= '9' {
+				state = toksDot0
+				continue DataLoop
+			}
+			if c == 'e' || c == 'E' {
+				state = toksE
+				continue DataLoop
+			}
+
+			// need to rewind one character since this was non-numeric
+			dataPos--
+			tokenType = tknNumber
+			break DataLoop
+
+		case toksE:
+			numberIsNonInteger = true
+
+			if c == '+' || c == '-' {
+				state = toksESign
+				continue DataLoop
+			}
+			fallthrough
+
+		case toksESign:
+			if '0' <= c && c <= '9' {
+				state = toksE0
+				continue DataLoop
+			}
+			return tknUnknown, nil, errors.New("in exponent of numeric literal")
+
+		case toksE0:
+			if '0' <= c && c <= '9' {
+				// continue parsing numbers...
+				continue DataLoop
+			}
+
+			// need to rewind one character since this was non-numeric
+			dataPos--
+			tokenType = tknNumber
+			break DataLoop
+
+		case toksT:
+			if c == 'r' {
+				state = toksTr
+				continue DataLoop
+			}
+			return tknUnknown, nil, errors.New("in literal true (expecting 'r')")
+
+		case toksTr:
+			if c == 'u' {
+				state = toksTru
+				continue DataLoop
+			}
+			return tknUnknown, nil, errors.New("in literal true (expecting 'u')")
+
+		case toksTru:
+			if c == 'e' {
+				tokenType = tknTrue
+				break DataLoop
+			}
+			return tknUnknown, nil, errors.New("in literal true (expecting 'e')")
+
+		case toksF:
+			if c == 'a' {
+				state = toksFa
+				continue DataLoop
+			}
+			return tknUnknown, nil, errors.New("in literal false (expecting 'a')")
+
+		case toksFa:
+			if c == 'l' {
+				state = toksFal
+				continue DataLoop
+			}
+			return tknUnknown, nil, errors.New("in literal false (expecting 'l')")
+
+		case toksFal:
+			if c == 's' {
+				state = toksFals
+				continue DataLoop
+			}
+			return tknUnknown, nil, errors.New("in literal false (expecting 's')")
+
+		case toksFals:
+			if c == 'e' {
+				tokenType = tknFalse
+				break DataLoop
+			}
+			return tknUnknown, nil, errors.New("in literal false (expecting 'e')")
+
+		case toksN:
+			if c == 'u' {
+				state = toksNu
+				continue DataLoop
+			}
+			return tknUnknown, nil, errors.New("in literal null (expecting 'u')")
+
+		case toksNu:
+			if c == 'l' {
+				state = toksNul
+				continue DataLoop
+			}
+			return tknUnknown, nil, errors.New("in literal null (expecting 'l')")
+
+		case toksNul:
+			if c == 'l' {
+				tokenType = tknNull
+				break DataLoop
+			}
+			return tknUnknown, nil, errors.New("in literal null (expecting 'l')")
+
 		}
 	}
+
+	// Enhance some of the tokens with additional information
+	if tokenType == tknNumber && !numberIsNonInteger {
+		tokenType = tknInteger
+	}
+	if tokenType == tknEscString && !strHasEscapes {
+		tokenType = tknString
+	}
+
+	endPos := dataPos
+	tokenData := tkn.data[startPos:endPos]
+
+	// Update the scanners state
+	tkn.pos = dataPos
+
+	return tokenType, tokenData, nil
+
 }
---
+++ jsontokenizer_test.go
@@ -0,0 +1,214 @@
+package gojsonsm
+
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"testing"
+)
+
+func testTokenizedStep(t *testing.T, tok *jsonTokenizer, expectedToken tokenType, expectedTokenStr string) {
+	t.Helper()
+
+	token, tokenData, err := tok.Step()
+	if err != nil {
+		t.Fatalf("encountered stepping error: %s", err)
+	}
+
+	if token != expectedToken {
+		t.Fatalf("Expected token `%s` but got `%s`", tokenToText(expectedToken), tokenToText(token))
+	}
+
+	if bytes.Compare(tokenData, []byte(expectedTokenStr)) != 0 {
+		t.Fatalf("Expected token data `%s` but got `%s`", expectedTokenStr, tokenData)
+	}
+}
+
+func testTokenizedValue(t *testing.T, dataBytes []byte, token tokenType, tokenData string) {
+	t.Helper()
+
+	// Set up the tokenizer
+	var tok jsonTokenizer
+	tok.Reset(dataBytes)
+
+	// Check that we receive the correct token
+	testTokenizedStep(t, &tok, token, tokenData)
+
+	// Check that we get end after the token
+	testTokenizedStep(t, &tok, tknEnd, "")
+}
+
+func testTokenizedValueEx(t *testing.T, data string, token tokenType) {
+	t.Helper()
+
+	dataBytes := []byte(data)
+	var testValue []byte
+
+	WHITESPACEBYTES := []byte("  \t  \n ")
+
+	// Check normally
+	testValue = dataBytes
+	testTokenizedValue(t, testValue, token, data)
+
+	// Check with whitespace ahead of it
+	testValue = append(WHITESPACEBYTES, dataBytes...)
+	testTokenizedValue(t, testValue, token, data)
+
+	// Check with whitespace after of it
+	testValue = append(dataBytes, WHITESPACEBYTES...)
+	testTokenizedValue(t, testValue, token, data)
+}
+
+func TestTokenizerSeeking(t *testing.T) {
+	dataBytes := []byte(`{
+		"a": "5b47eb0936ff92a567a0307e",
+		"b": false
+	}`)
+
+	var tok jsonTokenizer
+	tok.Reset(dataBytes)
+
+	testTokenizedStep(t, &tok, tknObjectStart, "{")
+	testTokenizedStep(t, &tok, tknString, `"a"`)
+
+	savedPos := tok.Position()
+	testTokenizedStep(t, &tok, tknObjectKeyDelim, ":")
+
+	tok.Seek(savedPos)
+	testTokenizedStep(t, &tok, tknObjectKeyDelim, ":")
+	testTokenizedStep(t, &tok, tknString, `"5b47eb0936ff92a567a0307e"`)
+}
+
+func TestTokenizeObject(t *testing.T) {
+	dataBytes := []byte(`{
+		"a": "5b47eb0936ff92a567a0307e",
+		"b": false
+	}`)
+
+	var tok jsonTokenizer
+	tok.Reset(dataBytes)
+
+	testTokenizedStep(t, &tok, tknObjectStart, "{")
+	testTokenizedStep(t, &tok, tknString, `"a"`)
+	testTokenizedStep(t, &tok, tknObjectKeyDelim, ":")
+	testTokenizedStep(t, &tok, tknString, `"5b47eb0936ff92a567a0307e"`)
+	testTokenizedStep(t, &tok, tknListDelim, ",")
+	testTokenizedStep(t, &tok, tknString, `"b"`)
+	testTokenizedStep(t, &tok, tknObjectKeyDelim, ":")
+	testTokenizedStep(t, &tok, tknFalse, `false`)
+	testTokenizedStep(t, &tok, tknObjectEnd, "}")
+	testTokenizedStep(t, &tok, tknEnd, "")
+}
+
+func TestTokenizeArray(t *testing.T) {
+	dataBytes := []byte(`[
+		1,
+		2999.22,
+		null,
+		"hello\u2932world"
+	]`)
+
+	var tok jsonTokenizer
+	tok.Reset(dataBytes)
+
+	testTokenizedStep(t, &tok, tknArrayStart, "[")
+	testTokenizedStep(t, &tok, tknInteger, "1")
+	testTokenizedStep(t, &tok, tknListDelim, ",")
+	testTokenizedStep(t, &tok, tknNumber, "2999.22")
+	testTokenizedStep(t, &tok, tknListDelim, ",")
+	testTokenizedStep(t, &tok, tknNull, "null")
+	testTokenizedStep(t, &tok, tknListDelim, ",")
+	testTokenizedStep(t, &tok, tknEscString, `"hello\u2932world"`)
+	testTokenizedStep(t, &tok, tknArrayEnd, "]")
+	testTokenizedStep(t, &tok, tknEnd, "")
+}
+
+func TestTokenizeString(t *testing.T) {
+	testTokenizedValueEx(t, `"lol"`, tknString)
+}
+
+func TestTokenizeEscString(t *testing.T) {
+	testTokenizedValueEx(t, `"l\nol"`, tknEscString)
+	testTokenizedValueEx(t, `"l\u2321ol"`, tknEscString)
+}
+
+func TestTokenizeInteger(t *testing.T) {
+	testTokenizedValueEx(t, `0`, tknInteger)
+	testTokenizedValueEx(t, `123`, tknInteger)
+	testTokenizedValueEx(t, `4565464651846548`, tknInteger)
+}
+
+func TestTokenizeNumber(t *testing.T) {
+	testTokenizedValueEx(t, `0.1`, tknNumber)
+	testTokenizedValueEx(t, `1999.1`, tknNumber)
+	testTokenizedValueEx(t, `14.29438383`, tknNumber)
+	testTokenizedValueEx(t, `1.0E+2`, tknNumber)
+	testTokenizedValueEx(t, `1.9e+22`, tknNumber)
+}
+
+func TestTokenizeBool(t *testing.T) {
+	testTokenizedValueEx(t, `true`, tknTrue)
+	testTokenizedValueEx(t, `false`, tknFalse)
+}
+
+func TestTokenizeNull(t *testing.T) {
+	testTokenizedValueEx(t, `null`, tknNull)
+}
+
+func TestTokenizeEndsForever(t *testing.T) {
+	dataBytes := []byte(`"hello world"`)
+
+	var tok jsonTokenizer
+	tok.Reset(dataBytes)
+
+	testTokenizedStep(t, &tok, tknString, `"hello world"`)
+	testTokenizedStep(t, &tok, tknEnd, "")
+	testTokenizedStep(t, &tok, tknEnd, "")
+}
+
+func TestTokenizerLong(t *testing.T) {
+	dataBytes, err := ioutil.ReadFile("testdata/people.json")
+	if err != nil {
+		panic(fmt.Sprintf("failed to read test data file: %s", err))
+	}
+
+	var tok jsonTokenizer
+	tok.Reset(dataBytes)
+	for {
+		token, _, err := tok.Step()
+		if err != nil {
+			panic(fmt.Sprintf("encountered stepping error: %s", err))
+		}
+
+		if token == tknEnd {
+			break
+		}
+	}
+}
+
+func BenchmarkTokenize(b *testing.B) {
+	var tok jsonTokenizer
+
+	dataBytes, err := ioutil.ReadFile("testdata/people.json")
+	if err != nil {
+		panic(fmt.Sprintf("failed to read test data file: %s", err))
+	}
+
+	b.SetBytes(int64(len(dataBytes)))
+	b.ResetTimer()
+
+	for j := 0; j < b.N; j++ {
+		tok.Reset(dataBytes)
+
+		for {
+			token, _, err := tok.Step()
+			if err != nil {
+				panic(fmt.Sprintf("encountered stepping error: %s", err))
+			}
+
+			if token == tknEnd {
+				break
+			}
+		}
+	}
+}
