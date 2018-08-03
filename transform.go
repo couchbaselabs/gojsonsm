05d007297ce48efc3c50df68dfcfb66b0da84600
Brett Lawson <brett19@gmail.com>
2018-08-03 03:48:14 -0700

    Moved MatcherDef and related types to their own file.
    The Transformer file was getting rather large, and the
    MatcherDef lives somewhat on its own anyways.

--- transform.go
+++ transform.go
@@ -3,22 +3,8 @@
 package gojsonsm
 
 import (
-	"fmt"
 	"regexp"
-	"sort"
-	"strings"
 )
-
-type SlotID int
-type BucketID int
-
-type SlotRef struct {
-	Slot SlotID
-}
-
-func (ref SlotRef) String() string {
-	return fmt.Sprintf("$%d", ref.Slot)
-}
 
 type mergeExpr struct {
 	exprs     []Expression
@@ -60,197 +46,6 @@ type Transformer struct {
 	CurDepth        int
 }
 
-type OpType int
-
-const (
-	OpTypeEquals OpType = iota
-	OpTypeNotEquals
-	OpTypeLessThan
-	OpTypeLessEquals
-	OpTypeGreaterThan
-	OpTypeGreaterEquals
-	OpTypeExists
-	OpTypeIn
-	OpTypeMatches
-)
-
-func opTypeToString(value OpType) string {
-	switch value {
-	case OpTypeEquals:
-		return "eq"
-	case OpTypeNotEquals:
-		return "neq"
-	case OpTypeLessThan:
-		return "lt"
-	case OpTypeLessEquals:
-		return "le"
-	case OpTypeGreaterThan:
-		return "gt"
-	case OpTypeGreaterEquals:
-		return "gte"
-	case OpTypeIn:
-		return "in"
-	case OpTypeExists:
-		return "exists"
-	}
-
-	return "??unknown??"
-}
-
-type OpNode struct {
-	BucketIdx BucketID
-	Op        OpType
-	Rhs       interface{}
-}
-
-func (op OpNode) String() string {
-	var out string
-	out += fmt.Sprintf("[%d] %s", op.BucketIdx, opTypeToString(op.Op))
-
-	if op.Rhs != nil {
-		out += " " + fmt.Sprintf("%v", op.Rhs)
-	}
-
-	return out
-}
-
-type LoopType int
-
-const (
-	LoopTypeAny LoopType = iota
-	LoopTypeEvery
-	LoopTypeAnyEvery
-)
-
-func loopTypeToString(value LoopType) string {
-	switch value {
-	case LoopTypeAny:
-		return "any"
-	case LoopTypeEvery:
-		return "every"
-	case LoopTypeAnyEvery:
-		return "anyevery"
-	}
-
-	return "??unknown??"
-}
-
-type LoopNode struct {
-	BucketIdx BucketID
-	Mode      LoopType
-	Node      *ExecNode
-}
-
-type ExecNode struct {
-	StoreId SlotID
-	Ops     []*OpNode
-	Elems   map[string]*ExecNode
-	Loops   []LoopNode
-	After   map[SlotID]*ExecNode
-}
-
-type MatchDef struct {
-	ParseNode    *ExecNode
-	MatchTree    binTree
-	MatchBuckets []int
-	NumBuckets   int
-	NumSlots     int
-	MaxDepth     int
-}
-
-func (def MatchDef) String() string {
-	var out string
-	out += "match tree:\n"
-	out += reindentString(def.ParseNode.String(), "  ")
-	out += "\n"
-	out += "bin tree:\n"
-	out += reindentString(def.MatchTree.String(), "  ")
-	out += "\n"
-	out += "match buckets:\n"
-	for i, bucketID := range def.MatchBuckets {
-		out += fmt.Sprintf("  %d: %d\n", i, bucketID)
-	}
-	out += fmt.Sprintf("num buckets: %d\n", def.NumBuckets)
-	out += fmt.Sprintf("num fetches: %d\n", def.NumSlots)
-	out += fmt.Sprintf("max depth: %d\n", def.MaxDepth)
-	return strings.TrimRight(out, "\n")
-}
-
-func (node *ExecNode) makeStored(t *Transformer) SlotID {
-	if node.StoreId == 0 {
-		node.StoreId = t.newSlot()
-	}
-	return node.StoreId
-}
-
-func (node *ExecNode) makeAfterNode(varID SlotID) *ExecNode {
-	if node.After == nil {
-		node.After = make(map[SlotID]*ExecNode)
-	} else {
-		foundNode := node.After[varID]
-		if foundNode != nil {
-			return foundNode
-		}
-	}
-
-	newNode := &ExecNode{}
-	node.After[varID] = newNode
-	return newNode
-}
-
-func (node ExecNode) String() string {
-	var out string
-	if node.StoreId > 0 {
-		out += fmt.Sprintf(":store $%d\n", node.StoreId)
-	}
-
-	if len(node.Ops) > 0 {
-		out += fmt.Sprintf(":ops\n")
-		for _, op := range node.Ops {
-			out += reindentString(op.String(), "  ")
-			out += "\n"
-		}
-	}
-
-	// For debugging, lets sort the elements by name first
-	var ks []string
-	for k := range node.Elems {
-		ks = append(ks, k)
-	}
-	sort.Strings(ks)
-	if len(ks) > 0 {
-		out += fmt.Sprintf(":elems\n")
-
-		for _, k := range ks {
-			elem := node.Elems[k]
-			out += fmt.Sprintf("  `%s`:\n", k)
-			out += reindentString(elem.String(), "    ")
-			out += "\n"
-		}
-	}
-
-	if node.Loops != nil {
-		out += fmt.Sprintf(":loops\n")
-		for _, loop := range node.Loops {
-			out += fmt.Sprintf("[%d] :%s:\n", loop.BucketIdx, loopTypeToString(loop.Mode))
-
-			out += reindentString(loop.Node.String(), "  ")
-			out += "\n"
-		}
-	}
-
-	if node.After != nil {
-		out += fmt.Sprintf(":after:\n")
-		for varId, anode := range node.After {
-			out += fmt.Sprintf("  #with $%d:\n", varId)
-			out += reindentString(anode.String(), "    ")
-			out += "\n"
-		}
-	}
-
-	return strings.TrimRight(out, "\n")
-}
-
 func (t *Transformer) getExecNode(field FieldExpr) *ExecNode {
 	node := t.RootExec
 
@@ -275,6 +70,28 @@ func (t *Transformer) getExecNode(field FieldExpr) *ExecNode {
 		node = newNode
 	}
 	return node
+}
+
+func (t *Transformer) storeNode(node *ExecNode) SlotID {
+	if node.StoreId == 0 {
+		node.StoreId = t.newSlot()
+	}
+	return node.StoreId
+}
+
+func (t *Transformer) makeAfterNode(node *ExecNode, slot SlotID) *ExecNode {
+	if node.After == nil {
+		node.After = make(map[SlotID]*ExecNode)
+	} else {
+		foundNode := node.After[slot]
+		if foundNode != nil {
+			return foundNode
+		}
+	}
+
+	newNode := &ExecNode{}
+	node.After[slot] = newNode
+	return newNode
 }
 
 func (t *Transformer) newBucket() BucketID {
@@ -386,8 +203,8 @@ func (t *Transformer) transformLoop(loopType LoopType, varID VariableID, inExpr,
 		// resolved and are available.
 		subRootRefs := subExpr.RootRefs()
 		if len(subRootRefs) > 0 {
-			storeId := execNode.makeStored(t)
-			execNode = t.ActiveExec.makeAfterNode(storeId)
+			storeId := t.storeNode(execNode)
+			execNode = t.makeAfterNode(t.ActiveExec, storeId)
 		}
 
 		execNode.Loops = append(execNode.Loops, LoopNode{
@@ -432,7 +249,7 @@ func (t *Transformer) transformAnyEveryIn(expr AnyEveryInExpr) *ExecNode {
 func (t *Transformer) makeRhsParam(expr Expression) interface{} {
 	if rhsField, ok := expr.(FieldExpr); ok {
 		rhsNode := t.getExecNode(rhsField)
-		rhsStoreId := rhsNode.makeStored(t)
+		rhsStoreId := t.storeNode(rhsNode)
 		return SlotRef{rhsStoreId}
 	} else if rhsValue, ok := expr.(ValueExpr); ok {
 		val := NewFastVal(rhsValue.Value)
@@ -481,8 +298,8 @@ func (t *Transformer) transformComparison(op OpType, lhs, rhs Expression) *ExecN
 
 		lhsRootRefs := rhs.RootRefs()
 		if len(lhsRootRefs) > 0 {
-			storeId := execNode.makeStored(t)
-			execNode = t.ActiveExec.makeAfterNode(storeId)
+			storeId := t.storeNode(execNode)
+			execNode = t.makeAfterNode(t.ActiveExec, storeId)
 		}
 
 		execNode.Ops = append(execNode.Ops, &OpNode{
